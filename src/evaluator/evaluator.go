5ab4674a1c9de247300191b8b61bb813305c9d0b
salty-max <max@jellycat.fr>
2024-06-15 21:31:55 +0200

    feat: eval if expressions

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -20,6 +20,10 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 
 		// Expressions
 	case *ast.IntegerLiteral:
@@ -50,6 +54,17 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return res
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
@@ -198,4 +213,17 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 		return TRUE
 	}
 	return FALSE
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
 }
